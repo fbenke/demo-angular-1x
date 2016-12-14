e170965deb21cec47f6320ba46aa2b4888d895dd
fbenke <falk@meltwater.org>
2016-12-14 13:24:34 +0100

    Add functionality to create profile on

--- app/app.js
+++ app/app.js
@@ -17,4 +17,15 @@ var app = angular.module('myWorkout', [
 .run(['$rootScope', 'auth', 'session', function($rootScope, auth, session){
   $rootScope.auth = auth;
   $rootScope.session = session;	
-}]);
+}])
+.run(function(){
+    var config = {
+      apiKey: "AIzaSyDCgvKiWSLIIRQNi56jWAux7QCYqMAqQUg",
+      authDomain: "myworkout-dc599.firebaseapp.com",
+      databaseURL: "https://myworkout-dc599.firebaseio.com",
+      storageBucket: "myworkout-dc599.appspot.com",
+      messagingSenderId: "624819308134"
+    };
+    firebase.initializeApp(config);
+  }
+);
--- app/profile/profile.js
+++ app/profile/profile.js
@@ -1,19 +1,38 @@
 'use strict';
- 
+
 angular.module('myWorkout.profile', ['ngRoute'])
- 
+
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/profile', {
-      templateUrl: 'profile/profile.html',
-      controller: 'ProfileCtrl'
+    templateUrl: 'profile/profile.html',
+    controller: 'ProfileCtrl'
   });
 }])
 
 
-.controller('ProfileCtrl', ['$location', 'auth', function($location, auth) {
+.controller('ProfileCtrl', ['$location', 'auth', 'session', '$scope', function($location, auth, session, $scope) {
 
   if(!auth.isLoggedIn()){
     $location.path('/login');
   }
-}]); 
 
+  $scope.profileName = session.getUser().name;
+
+
+  $scope.edit = function() {
+
+    var fb = firebase.database().ref('Profiles/');
+
+    fb.push({
+      name: $scope.profileName,
+      id: session.getUser().id,
+      age: $scope.profileAge,
+      '.priority': session.getUser().id
+    }).then(function(ref) {
+      $location.path('/welcome');
+    }, function(error) {
+      console.log("Error:", error);
+    });
+  };
+
+}]);
